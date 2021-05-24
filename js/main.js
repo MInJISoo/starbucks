560c5c77c949b6357eff1f18ae9e18266cd79083
MinJiSoo <mjparkm1111@gmail.com>
2021-05-24 20:53:40 +0900

    Start project

---
+++ js/main.js
@@ -0,0 +1,172 @@
+const searchEl = document.querySelector('.search');
+const searchInputEl = searchEl.querySelector('input');
+
+searchEl.addEventListener('click', function () {
+  // Logic..
+  searchInputEl.focus();
+});
+
+searchInputEl.addEventListener('focus', function() {
+  searchEl.classList.add('focused');
+  searchInputEl.setAttribute('placeholder', '통합검색');  
+  //만약 search안에 input요소(위에서 querySelector로 찾아둔)가 focus되면 익명함수function()이 작동한다.
+  //익명함수> html의 search라는 클래스를 가진 요소에 'focused'라는 클래스리스트를 추가할거고,
+  //그리고 search안에 input이라는 요소 안에(위에서 querySelector로 찾아둔) 'placeholder'라는 '속성'을 추가할거다. 그 인수는 '통합검색'이다.
+});
+
+searchInputEl.addEventListener('blur', function() {
+  searchEl.classList.remove('focused');
+  searchInputEl.setAttribute('placeholder', '');
+  //한단락 위에서 searchInputEl이 focus되면 searchEl에 'focused'라는 classList가 추가되는데,
+  //searchInputEl의 focus가 blur됐을 때 searchEl의 classList에서 'focused'라는 클래스를 삭제하겠다.
+  //그리고 searchInputEl의 'placeholder'라는 속성의 값(인수)를 빈문자열 '' 로 세팅하겠다.
+});
+
+
+const badgeEl = document.querySelector('header .badges');
+const toTopEl = document.querySelector('#to-top');
+
+window.addEventListener('scroll', _.throttle(function () {
+  console.log(window.scrollY);
+  if (window.scrollY > 500) {
+    // badge 뱃지 숨기기
+    // gsap.to(요소, 지속시간, 옵션);
+    gsap.to(badgeEl, .6, {
+      opacity: 0,
+      display: 'none'
+    });
+    // To-top 버튼 보이기!
+    gsap.to(toTopEl, 0.2, {
+      x: 0
+    });
+  } else {
+    // badge 뱃지 보이기
+    gsap.to(badgeEl, .6, {
+      opacity: 1,
+      display: 'block'
+    });
+    // To-top 버튼 숨기기!
+    gsap.to(toTopEl, 0.2, {
+      x: 100
+    });
+  }
+}, 300));
+//  _.throttle(함수, ms단위의 시간)  < 이 기능을 사용하기 위해선 Lodash cdn검색해서 소스코드 복사, index.html파일의 main.js를 호출한 태그 윗줄에 소스코드 붙여넣기 해야한다.
+
+toTopEl.addEventListener('click', function() {
+  gsap.to(window, .7, {
+    scrollTo: 0
+  });
+});
+
+
+
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+fadeEls.forEach(function (fadeEl, index) {
+   // gsap.to(요소, 지속시간, 옵션);
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * .7,  // 0.7, 1.4, 2.1, 2.7초 뒤에 동작하도록 만듦. index는 카운트할 때 0부터 하기 때문에 + 1 하고 0.7초를 곱해줌.
+    opacity: 1
+  });
+  
+});
+
+
+
+// SWIPER
+// new Swiper(선택자, 옵션)
+new Swiper('.notice-line .swiper-container', {
+  direction: 'vertical',
+  autoplay: true,
+  loop: true
+});
+new Swiper('.promotion .swiper-container', {
+  // direction: 'horizontal', << 호리즌탈이 기본값이라 생략가능
+  slidesPerView: 3, // 한번에 보여줄 슬라이드 개수
+  spaceBetween: 10, // 슬라이드 사이 여백, 간격
+  centeredSlides: true, // 1번 슬라이드가 가운데 보이기. 왼쪽부터가 아니라.
+  loop: true,
+  autoplay: {
+    delay: 5000
+  },
+  pagination: {
+    el: '.promotion .swiper-pagination', // 페이지 번호 요소 선택자
+    clickable: true //사용자의 페이지 번호 요소 제어 가능 여부
+  },
+  navigation: {
+    prevEl: '.promotion .swiper-prev',
+    nextEl: '.promotion .swiper-next'
+  }
+});
+new Swiper('.awards .swiper-container', {
+  // direction: 'horizontal'  <<기본값이라 생략가능
+  autoplay: true,          // 자동으로 슬라이드 넘어가게 할거냐 ㅇ 
+  loop: true,          // 슬라이드 넘김을 무한반복 할거냐 ㅇ
+  spaceBetween: 30,    // 슬라이드 간격
+  slidesPerView: 5,     // 한 화면에 보여줄 슬라이드 갯수
+  navigation: {
+    prevEl: '.awards .swiper-prev',
+    nextEl: '.awards .swiper-next'
+  }
+});
+
+
+
+
+const promotionEl = document.querySelector('.promotion');
+const promotionToggleBtn = document.querySelector('.toggle-promotion');
+let isHidePromotion = false;
+promotionToggleBtn.addEventListener('click', function () {
+  isHidePromotion = !isHidePromotion
+  if (isHidePromotion) {
+    // 숨김 처리!
+    promotionEl.classList.add('hide');
+  } else {
+    // 보임 처리!
+    promotionEl.classList.remove('hide');
+  }
+});
+
+// 범위 랜덤 함수(소수점 2자리까지)
+function random(min, max) {
+  // `.toFixed()`를 통해 반환된 문자 데이터를,
+  // `parseFloat()`을 통해 소수점을 가지는 숫자 데이터로 변환
+  return parseFloat((Math.random() * (max - min) + min).toFixed(2))
+}
+function floatingObject(selector, delay, size) {
+  // gsap.to(요소, 시간, 옵션);
+  gsap.to(
+    selector, 
+    random(1.5, 2.5), 
+    {
+      y: size,
+      repeat: -1,       // 이 외부js라이브러리에서 제공하는 값. -1 = '무한반복' 
+      yoyo: true,
+      ease: Power1.easeInOut,
+      delay: random(0, delay)
+    }
+  );
+}
+floatingObject('.floating1', 1, 15);
+floatingObject('.floating2', .5, 15);
+floatingObject('.floating3', 1.5, 20);
+
+
+// ScrollMagic cdn 외부 JS라이브러리 
+const spyEls = document.querySelectorAll('section.scroll-spy');
+spyEls.forEach(function (spyEl) {
+  new ScrollMagic
+    .Scene({
+      triggerElement: spyEl,  //보여짐의 여부를 감시할 요소를 지정 
+      triggerHook: .8
+    })
+    .setClassToggle(spyEl, 'show')
+    .addTo(new ScrollMagic.Controller());
+});
+
+
+
+//Copyright 의 년도(Year)를 매년 바꿔줄 수 없으니까 자동으로 현재년도로 맞춰주는 코드.
+const thisYear = document.querySelector('.this-year');
+thisYear.textContent = new Date().getFullYear(); // 올해의 년도 숫자가 나옴 ex)2021
+
